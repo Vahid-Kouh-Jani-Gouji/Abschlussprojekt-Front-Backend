71220f478759796ec75d5c12955a550f22bf01de
vahid <vk1604@yahoo.com>
2024-01-18 09:33:09 +0100

    teting init

---
+++ test/Counter.test.js
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Counter from "../src/Counter";
+
+describe("Counter", () => {
+  it("should count", () => {
+    render(<Counter />);
+    const countButton = screen.getByText("count is: 0");
+    countButton.click();
+    screen.getByText("count is: 1");
+  });
+});
+
